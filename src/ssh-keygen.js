9370cb2a3ae09ba759db63e74c44d459b3b97b5e
Carlos Rodriguez <carlos@s8f.org>
2014-03-28 00:54:44 -0700

    Fix stuff

--- src/ssh-keygen.js
+++ src/ssh-keygen.js
@@ -5,79 +5,75 @@ var os = require('os');
 var path = require('path');
 
 var log = function(a){
-	if(process.env.VERBOSE) console.log('ssh-keygen: '+a);
+  if (process.env.VERBOSE) console.log('ssh-keygen: ' + a);
 }
 
-function ssh_keygen(location, opts, callback){
-	opts || (opts={});
+function ssh_keygen(location, opts, callback) {
+  opts || (opts = {});
 
-	var pubLocation = location+'.pub';
-	if(!opts.comment) opts.comment = '';
-	if(!opts.password) opts.password = '';
+  var pubLocation = location + '.pub'
+    , args;
 
-	var keygen = spawn('ssh-keygen', [
-		'-t','rsa',
-		'-b', opts.bits || '2048',
-		'-C', opts.comment,
-		'-N', opts.password,
-		'-f', location,
-		opts.fingerprint ? '-l' : ''
-	]);
+  if (!opts.comment) opts.comment = '';
+  if (!opts.password) opts.password = '';
 
-	if (opts.fingerprint) {
-		var fingerprint = '';
-		keygen.stdout.on('end', function () {
-			callback(null, fingerprint);
-		});
+  if (opts.fingerprint) {
+  	args = [
+  	'-lf', location
+  	];
+  }
+  else {
+	  	args = [
+	    '-t','rsa',
+	    '-b', opts.bits || '2048',
+	    '-C', opts.comment,
+	    '-N', opts.password,
+	    '-f', location
+	  ];
 	}
 
-	keygen.stdout.on('data', function(a){
-		log('stdout:'+a);
-		if (opts.fingerprint) fingerprint = fingerprint + a;
-	});
+  var keygen = spawn('ssh-keygen', args);
 
-	var read = opts.read;
-	var destroy = opts.destroy;
+  if (opts.fingerprint) {
+    var fingerprint = '';
+    keygen.stdout.on('end', function () {
+      callback(null, fingerprint);
+    });
+  }
 
-	keygen.on('exit',function(){
-		if (opts.fingerprint) return;
-		log('exited');
-		if(read){
-			log('reading key '+location);
-			fs.readFile(location, 'utf8', function(err, key){			
-				if(destroy){
-					log('destroying key '+location);
-					fs.unlink(location, function(err){
-						if(err) return callback(err);
-						readPubKey();
-					});
-				} else readPubKey();
-				function readPubKey(){
-					log('reading pub key '+pubLocation);
-					fs.readFile(pubLocation, 'utf8', function(err, pubKey){
-						if(destroy){
-							log('destroying pub key '+pubLocation);
-							fs.unlink(pubLocation, function(err){
-								if(err) return callback(err);
-								return callback(undefined, { key: key, pubKey: pubKey });
-							});
-						} else callback(undefined, { key: key, pubKey: pubKey });
-					});
-				}
-			});
-		} else if(callback) callback();
-	});
+  keygen.stdout.on('data', function(a){
+    log('stdout:'+a);
+    if (opts.fingerprint) fingerprint = fingerprint + a;
+  });
 
-	keygen.stderr.on('data',function(a){
-		log('stderr:'+a);
-	});
+  var read = opts.read;
+  var destroy = opts.destroy;
+
+  keygen.on('exit',function(){
+    if (opts.fingerprint) return;
+    log('exited');
+    if (read) {
+      log('reading key '+location);
+      fs.readFile(location, 'utf8', function(err, key) {     
+        log('reading pub key ' + pubLocation);
+        fs.readFile(pubLocation, 'utf8', function(err, pubKey) {
+          callback(null, { key: key, pubKey: pubKey });
+        });
+      });
+    } else if (callback) callback();
+  });
+
+  keygen.stderr.on('data',function(a){
+    log('stderr:'+a);
+    fingerprint = fingerprint + a;
+  });
 };
 
 module.exports = function(opts, callback){
-	var location = opts.location;
-	if(!location) location = path.join(os.tmpDir(),'id_rsa');
+  var location = opts.location;
+  if (!location) location = path.join(os.tmpDir(),'id_rsa');
 
-	if(_.isUndefined(opts.read)) opts.read = true;
+  if (typeof opts.read === 'undefined') opts.read = true;
 
-	ssh_keygen(location, opts, callback);
+  ssh_keygen(location, opts, callback);
 };
